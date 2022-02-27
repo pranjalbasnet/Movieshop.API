aa829cdf10f9d5d646a2391311e7ab0e6793f0a2
Pranjal Basnet <33705708+pranjalbasnet@users.noreply.github.com>
2022-02-27 17:13:38 -0600

    files commited

---
+++ MovieshopApp/Movieshop.API/Startup.cs
@@ -0,0 +1,26 @@
+﻿namespace Movieshop.API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection service)
+        {
+            service.AddControllers();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
